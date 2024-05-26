4656496984b0999d0cda5b0febf1674b7600e78a
Samudini-D <dksdoraliyagoda@gmail.com>
2024-05-26 11:58:50 -0700

    part_1

---
+++ part_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define MAX_STACK_SIZE 100
+
+typedef struct {
+    double data[MAX_STACK_SIZE];
+    int top;
+} Stack;
+
+void initStack(Stack *s) {
+    s->top = -1;
+}
+
+int isFull(Stack *s) {
+    return s->top == MAX_STACK_SIZE - 1;
+}
+
+int isEmpty(Stack *s) {
+    return s->top == -1;
+}

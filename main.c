d6ca50d1a6e5a50eb74f5cadc51320447d9a76ef
Deesara Rupasinghe <119430923+DeesaraRupasinghe@users.noreply.github.com>
2024-05-29 15:32:17 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,26 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define MAX_STACK_SIZE 100
+
+typedef struct {
+    double data[MAX_STACK_SIZE];
+    int top;
+} Stack;
+
+void initStack(Stack *s) {
+    s->top = -1;
+}
+
+int isFull(Stack *s) {
+    return s->top == MAX_STACK_SIZE - 1;
+}
+
+int isEmpty(Stack *s) {
+    return s->top == -1;
+}
 void push(Stack *s, double value) {
     if (isFull(s)) {
         printf("Stack overflow\n");

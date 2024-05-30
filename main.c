f3af9e83985afe82c9a682aa126eaccac70d7616
Ravindu Wijekoon <140045564+Ravindua-Wijekoon@users.noreply.github.com>
2024-05-30 23:46:41 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,1 @@
 
-void push(Stack *s, double value) {
-    if (isFull(s)) {
-        printf("Stack overflow\n");
-        exit(1);
-    }
-    s->data[++s->top] = value;
-}
-
-double pop(Stack *s) {
-    if (isEmpty(s)) {
-        printf("Stack underflow\n");
-        exit(1);
-    }
-    return s->data[s->top--];
-}
